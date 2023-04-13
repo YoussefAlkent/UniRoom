11589b269392b98962de1e319867a96ac22bd12b
SpectreWasTaken <youssef.bedair@aiu.edu.eg>
2023-04-13 02:19:47 +0200

    Login and signup done, created skeleton for post login and signup, added serveralt.js to test with

--- serveralt.js
+++ serveralt.js
@@ -3,6 +3,7 @@ const ejs = require('ejs')
 const express = require('express');
 const mysql = require('mysql');
 const fs = require('fs');
+const bodyParser = require('body-parser')
 var con = mysql.createConnection({
     host: "uni-room.mysql.database.azure.com",
     port: 3306,
@@ -12,20 +13,19 @@ var con = mysql.createConnection({
 });
 
 var app = express();
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '\\views\\pages');
 
 app.listen(8080);
 app.use(express.static('css'));
 app.use(express.static('js'));
-
 app.get('/', (req, res)=>{
-    res.render('signintemp')
+    res.render('signup')
 })
 
 
-app.post('/login', (res, req) =>{
-    var name = req.body.name
-    var pass = req.body.password
-    console.log(name, "     ", pass)
+app.post('/signup', async (req, res) =>{
+    console.log(req.body)
 })
